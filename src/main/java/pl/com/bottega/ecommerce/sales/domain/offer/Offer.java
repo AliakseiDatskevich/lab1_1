6a8c75911596e0c89b6e35a374dfb2e352346c29
Aliaksei Datskevich <187672@edu.p.lodz.pl>
2018-01-12 22:25:54 +0100

    Rafactorized code

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Offer.java
@@ -4,87 +4,96 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Offer {
-	private List<OfferItem> availableItems = new ArrayList<OfferItem>();
 
-	private List<OfferItem> unavailableItems = new ArrayList<OfferItem>();
+    private List<OfferItem> availableItems = new ArrayList<OfferItem>();
 
-	public Offer(List<OfferItem> availabeItems, List<OfferItem> unavailableItems) {
-		this.availableItems = availabeItems;
-		this.unavailableItems = unavailableItems;
-	}
+    private List<OfferItem> unavailableItems = new ArrayList<OfferItem>();
 
-	public Offer() {
-	};
+    public Offer(List<OfferItem> availabeItems, List<OfferItem> unavailableItems) {
+        this.availableItems = availabeItems;
+        this.unavailableItems = unavailableItems;
+    }
 
-	public List<OfferItem> getAvailabeItems() {
-		return availableItems;
-	}
+    public Offer() {};
 
-	public List<OfferItem> getUnavailableItems() {
-		return unavailableItems;
-	}
+    public List<OfferItem> getAvailabeItems() {
+        return availableItems;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((availableItems == null) ? 0 : availableItems.hashCode());
-		return result;
-	}
+    public List<OfferItem> getUnavailableItems() {
+        return unavailableItems;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Offer other = (Offer) obj;
-		if (availableItems == null) {
-			if (other.availableItems != null)
-				return false;
-		} else if (!availableItems.equals(other.availableItems))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (availableItems == null ? 0 : availableItems.hashCode());
+        return result;
+    }
 
-	/**
-	 * 
-	 * @param seenOffer
-	 * @param delta
-	 *            acceptable difference in percent
-	 * @return
-	 */
-	public boolean sameAs(Offer seenOffer, double delta) {
-		if (!(availableItems.size() == seenOffer.availableItems.size()))
-			return false;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Offer other = (Offer) obj;
+        if (availableItems == null) {
+            if (other.availableItems != null) {
+                return false;
+            }
+        } else if (!availableItems.equals(other.availableItems)) {
+            return false;
+        }
+        return true;
+    }
 
-		for (OfferItem item : availableItems) {
-			OfferItem sameItem = seenOffer.findItem(item.getProductId());
-			if (sameItem == null)
-				return false;
-			if (!sameItem.sameAs(item, delta))
-				return false;
-		}
+    /**
+     * 
+     * @param seenOffer
+     * @param delta
+     *            acceptable difference in percent
+     * @return
+     */
+    public boolean sameAs(Offer seenOffer, double delta) {
+        if (!(availableItems.size() == seenOffer.availableItems.size())) {
+            return false;
+        }
 
-		return true;
-	}
+        for (OfferItem item : availableItems) {
+            OfferItem sameItem = seenOffer.findItem(item.getProductId());
+            if (sameItem == null) {
+                return false;
+            }
+            if (!sameItem.sameAs(item, delta)) {
+                return false;
+            }
+        }
 
-	public void setUnavailableItems(List<OfferItem> unavailableItems) {
-		this.unavailableItems = unavailableItems;
-	}
+        return true;
+    }
 
-	private OfferItem findItem(String productId) {
-		for (OfferItem item : availableItems) {
-			if (item.getProductId().equals(productId))
-				return item;
-		}
-		return null;
-	}
+    public void setUnavailableItems(List<OfferItem> unavailableItems) {
+        this.unavailableItems = unavailableItems;
+    }
 
-	public void setAvailableItems(List<OfferItem> availableItems) {
-		this.availableItems = availableItems;
-	}
+    private OfferItem findItem(String productId) {
+        for (OfferItem item : availableItems) {
+            if (item.getProductId().equals(productId)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void setAvailableItems(List<OfferItem> availableItems) {
+        this.availableItems = availableItems;
+    }
 
 }
--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Test.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Test.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 public class Test {
 
-	public static void main(String[] args) {
-		Offer offerOne = new Offer();
-		List<OfferItem> availableItems = null;
-		offerOne.setAvailableItems(availableItems);
-		List<OfferItem> unavailableItems = null;
-		offerOne.setUnavailableItems(unavailableItems);
-		OfferItem offerItem1 = new OfferItem();
-		offerOne.getAvailabeItems().add(offerItem1);
-	}
-
+    public static void main(String[] args) {
+        Offer offerOne = new Offer();
+        List<OfferItem> availableItems = null;
+        offerOne.setAvailableItems(availableItems);
+        List<OfferItem> unavailableItems = null;
+        offerOne.setUnavailableItems(unavailableItems);
+        OfferItem offerItem1 = new OfferItem();
+        offerOne.getAvailabeItems().add(offerItem1);
+    }
 }
